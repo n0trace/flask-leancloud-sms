4b5aad6604b3168913ec0f9f3bdc7280afe8b9fc
ncitycode <ncitycode@gmail.com>
2017-05-01 10:50:11 +0000

    readme completed

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # flask-leancloud-sms
 
+## 安装
+
+```python
+pip install flask-leancloud-sms
+```
+
+## 配置
+
+| 配置项 | 说明 |
+|:--------------------:|:---------------------------:|
+| LEANCLOUD_APP_ID | 应用id |
+| LEANCLOUD_APP_KEY | 应用key |
+
+## 基本使用
+
+```python
+from flask import Flask
+from flask_leancloud_sms import Leancloud_Sms
+app = Flask(__name__)
+lsms= Leancloud_Sms(app)
+```
+
+## 发送短信验证码
+
+```python
+@app.route('/send-sms/<phone>')
+def send_sms(phone):
+    return lsms.send_message(phone)
+```
+
+## 发送语音验证码
+
+```python
+@app.route('/send-sms/<phone>')
+def send_sms(phone):
+    return lsms.send_message(phone,smsType='voice')
+```
+
+## 国际短信
+
+```python
+@app.route('/send-sms/<phone>')
+def send_sms(phone):
+    return lsms.send_message(phone,countryCode='CN')
+```
+
+> `countryCode` 参考 [https://blog.leancloud.cn/4818/](`https://blog.leancloud.cn/4818/`)
+
+## 自定义短信模板
+
+```python
+@app.route('/send-sms/<phone>')
+def send_sms(phone):
+    return lsms.send_message(phone,template='template_test',name=username,time='2017-5-1')
+```
+
+## 验证码验证
+
+```python
+@app.route('/verify-sms/<phone>/<code>')
+def verify(phone,code):
+    return lsms.verify_sms(phone,code)
+```
+
+## 函数返回值
+
+- `send_message` return `true` or `false`
+- `verify_sms` return `true` or `false`
--- flask_leancloud_sms.py
+++ flask_leancloud_sms.py
@@ -10,7 +10,6 @@ class Leancloud_Sms(object):
         self._app_key = app.config.get('LEANCLOUD_APP_KEY', '')
         self._request_sms_code_url = app.config.get('REQUEST_SMS_CODE_URL', 'https://api.leancloud.cn/1.1/requestSmsCode')
         self._verify_sms_code_url = app.config.get('VERIFY_SMS_CODE_URL', 'https://api.leancloud.cn/1.1/verifySmsCode/')
-        self._send_sms_type = app.config.get('SEND_SMS_TYPE', 'sms')
         
         self._headers = {
             "X-LC-Id": 'self._app_id',
@@ -20,8 +19,12 @@ class Leancloud_Sms(object):
     def send_message(phone,smsType='sms',countryCode='CN',template=None,**argv):
         """
         通过 POST 请求 requestSmsCode API 发送验证码到指定手机
-        :param phone: 通过网页表单获取的电话号
-        :return: 
+        :param phone: 电话号
+        :param smsType: 验证类型
+        :param countryCode: 国家类型
+        :param template: 短信模板
+        :param argv: 模板参数
+        :return: bool值
         """
         data = {
             "mobilePhoneNumber": phone,
@@ -46,15 +49,13 @@ class Leancloud_Sms(object):
     def verify_sms(phone, code):
         """
         发送 POST 请求到 verifySmsCode API 获取校验结果
-        :param phone: 通过网页表单获取的电话号
-        :param code: 通过网页表单获取的验证码
+        :param phone: 电话号
+        :param code: 验证码
         :return: 
         """
         # 使用传进的参数 code 与 phone 拼接出完整的 URL
         target_url = self._verify_sms_code_url + "%s?mobilePhoneNumber=%s" % (code, phone)
-        # 这里的 POST 方法只传入了两个参数
-        # target_url： 请求的 URL
-        # headers: 请求的头部，包含 Id 与 Key 等信息
+
         r = requests.post(target_url, headers=self._headers)
         # 响应 r 的 status_code 值为 200 说明验证成功
         # 否则失败
